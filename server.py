79b89a0344ea8d3d07ff42a621b078b50df098ca
Brady Trudel <bradytrudel@outlook.com>
2023-11-23 17:41:35 -0500

    working on online mode

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from ast import literal_eval
 
 # Klockcraft Java Edition 1.20 Server
 
-print('Starting Server...')
+print('[STARTING] Starting Server...')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_configs = literal_eval(open('configs/server_configs.json', 'r').read())
 world_configs = literal_eval(open('configs/world_configs.json', 'r').read())
@@ -11,6 +11,24 @@ world_configs = literal_eval(open('configs/world_configs.json', 'r').read())
 host = socket.gethostname()
 port = server_configs['port']
 player_limit = server_configs['player_limit']
+online_mode = server_configs['online_mode']
+
+if online_mode == 'true': online_mode = True
+else: online_mode = False
+
 s.bind((host, port))
 s.listen(player_limit)
-print(f'Started Server! Hosting on {host}, the server port is {port}.')
+print(f'[STARTED] Started Server! Hosting on {host}, the server port is {port}.')
+
+players_online = 0
+
+
+while True:
+    clientsocket, address = s.accept()
+
+    # check if the player is connecting from the same computer as the server for online mode
+    if online_mode:
+        pass
+    else:
+        if list(address)[0] != socket.gethostbyname(socket.gethostname()): clientsocket.close()
+    
